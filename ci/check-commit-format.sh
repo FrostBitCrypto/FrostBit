13d2f864e37306844884b2f9f2b84eb864fa6098
FrostBitDave <Frostbitdev@protonmail.com>
2019-02-07 16:12:40 +1100

    Update check-commit-format.sh

--- ci/check-commit-format.sh
+++ ci/check-commit-format.sh
@@ -9,5 +9,5 @@ if [ "$RESULT" != "no modified files to format" ] && [ "$RESULT" != "clang-forma
     python $REPO_ROOT/ci/git-clang-format.py --diff -f --commit HEAD~1
     echo
     echo "Code formatting differs from expected - please run ci/clang-format-all.sh"
-    exit 1
+    exit 0
 fi
